d9af8ecb8420634cae98a45b9b3cea13ba54a324
BarrelDev <visvam64@gmail.com>
2026-07-31 19:36:36 -0400

    added ability to pull from modrinth API

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
 use serde::{Deserialize, Serialize};
+use std::result::Result;
+use std::fmt;
+use std::error;
+
+#[derive(Debug, Clone)]
+struct InvalidSourceError;
+
+impl fmt::Display for InvalidSourceError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Invalid Mod Source")
+    }
+}
+
+impl error::Error for InvalidSourceError {}
 
 #[derive(Serialize, Deserialize)]
 struct Mod {
@@ -13,13 +27,31 @@ struct Config {
     mods: Vec<Mod>
 }
 
-fn main() {
+async fn pull_mod(mod_: &Mod) -> Result<(), Box<dyn error::Error>> {
+    if mod_.source != "modrinth" {
+        return Result::Err(InvalidSourceError.into());
+    }
+    let resp = reqwest::get(format!("https://staging-api.modrinth.com/v2/project/{}", mod_.id))
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
+    println!("{resp:#?}");
+    Result::Ok(())
+}
+
+#[tokio::main]
+async fn main() {
     println!("YAMM Test");
     let mut mods: Vec<Mod> = vec![];
     mods.push(Mod{
         id: String::from("fabric-api"),
         source: String::from("modrinth")
     });
+
+    if let Err(e) = pull_mod(&mods[0]).await {
+        eprintln!("failed to pull fabric-api: {e}");
+    }
+    
     mods.push(Mod{
         id: String::from("sodium"),
         source: String::from("modrinth")
